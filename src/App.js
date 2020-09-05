203d3195698c31ac730900760689625768a4357b
Mask Wolf <maskwolf2000@gmail.com>
2020-09-06 02:12:29 +0530

    add files

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import React from "react";
+import Home from "./Home";
+import Header from "./Header";
 
 function App() {
   return (
     <div className='App'>
       <h1>Let's do this!!</h1>
+      <Home></Home>
     </div>
   );
 }
